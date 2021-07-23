9a17fc99e68642552d461392ad921b14930af9b0
Alejandro Mongua <alejandro.mongua@gmail.com>
2021-07-22 22:16:21 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Backtester
+
+This project works as a backtester for my crypto trading algorithms based on the data on Binance platform
+
+Is beeing tested on python 3.9.5
+
+For this project to work, it needs a config.json file with the credentials to access Binance
+
+It downloads 1 minute candles from the begining of time and stores it into a local sqlite database in order to avoid making too many requests to binance platform
---
+++ klines.py
@@ -0,0 +1,45 @@
+from database import Database
+from kline import Kline
+
+
+class Klines():
+    def __init__(self):
+        self.current_index = 0
+        self.database = Database()
+
+    def generate_schema(self):
+        DEFAULT_SCHEMA = """
+        CREATE TABLE klines (id integer primary key autoincrement,
+            symbol char(20)
+            open_time float
+            open_price float
+            highest_price float
+            lowest_price float
+            close_price float
+            volume float
+            close_time float
+            quote_asset_volume float
+            number_of_trades integer
+            taker_buy_base_asset_volume float
+            taker_buy_quote_asset_volume float);
+        """
+
+        self.database.execute(DEFAULT_SCHEMA)
+        self.database.execute('CREATE INDEX klines_symbol ON klines (symbol)')
+        self.database.execute(
+            'CREATE INDEX klines_open_time ON klines (open_time)')
+
+    def next_kline(self):
+        self.database.execute('select * from klines where id > ? limit 1',
+                              (self.current_index,))
+
+        response = self.database.fetchone()
+        self.current_index, symbol, *kline_tuple = response
+        return Kline(symbol, kline_tuple)
+
+    def store(self, kline: Kline):
+        fields = kline.to_dict()
+        fields_names = ', '.join([f':{field}' for field in fields.keys()])
+        questions_marks = ', '.join('?' * len(fields.keys()))
+        self.database.execute(
+            f'insert into klines ({fields_names}) values ({questions_marks})', fields)
